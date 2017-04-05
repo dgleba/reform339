66b3b9fbd8ed2fe08c74ac464e729fea91f06be9
David Gleba <dgleba@gmail.com>
2017-04-05 18:56:07 -0400

    albums working like gogogarrett

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,74 +1,53 @@
 class AlbumsController < ApplicationController
-  before_action :set_album, only: [:show, :edit, :update, :destroy]
 
-  # GET /albums
-  # GET /albums.json
   def index
     @albums = Album.all
   end
 
-  # GET /albums/1
-  # GET /albums/1.json
   def show
+    @album = Album.find(params[:id])
   end
 
-  # GET /albums/new
   def new
-    @album = Album.new
+    # build random amount of songs
+    # (1+rand(7)).ceil.times do |i|
+    #   album.songs.build
+    # end
+    album = Album.new(songs: [Song.new, Song.new])
+    @form = Forms::AlbumForm.new(album)
   end
 
-  # GET /albums/1/edit
   def edit
+    album = Album.find(params[:id])
+    @form = Forms::AlbumForm.new(album)
   end
 
-  # POST /albums
-  # POST /albums.json
   def create
-    @album = Album.new(album_params)
+    # how you could do it dynamically..(for now)
+    # params[:album][:songs_attributes].size.times do |i|
+    #   album.songs.build
+    # end
+    album = Album.new(songs: [Song.new, Song.new])
+    @form = Forms::AlbumForm.new(album)
 
-    respond_to do |format|
-      if @album.save
-        format.html { redirect_to @album, notice: 'Album was successfully created.' }
-        format.json { render :show, status: :created, location: @album }
-      else
-        format.html { render :new }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
+    if @form.validate(params["album"])
+      @form.save
+      redirect_to album_path(album)
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /albums/1
-  # PATCH/PUT /albums/1.json
   def update
-    respond_to do |format|
-      if @album.update(album_params)
-        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
-        format.json { render :show, status: :ok, location: @album }
-      else
-        format.html { render :edit }
-        format.json { render json: @album.errors, status: :unprocessable_entity }
-      end
+    album = Album.find(params[:id])
+    @form = Forms::AlbumForm.new(album)
+
+    if @form.validate(params["album"])
+      @form.save
+      redirect_to album_path(album)
+    else
+      render :edit
     end
   end
 
-  # DELETE /albums/1
-  # DELETE /albums/1.json
-  def destroy
-    @album.destroy
-    respond_to do |format|
-      format.html { redirect_to albums_url, notice: 'Album was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_album
-      @album = Album.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def album_params
-      params.require(:album).permit(:title)
-    end
 end
